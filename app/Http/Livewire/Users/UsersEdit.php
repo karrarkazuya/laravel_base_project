0c6a50929689ccbf725f832cb56ef47d31a2cfd1
Karrar S. Honi <karrar.kazuya@gmail.com>
2021-08-07 13:00:25 +0300

    livewire add roles

--- app/Http/Livewire/Users/UsersEdit.php
+++ app/Http/Livewire/Users/UsersEdit.php
@@ -6,17 +6,20 @@ use App\Models\Team;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Spatie\Permission\Models\Role;
 
 class UsersEdit extends Component
 {
     public $user;
     public $teams;
+    public $roles;
 
     // used for the add form
     public $name = '';
     public $email = '';
     public $password = '';
     public $team = '';
+    public $role = '';
     public $phone = '';
     public $address = '';
 
@@ -25,16 +28,19 @@ class UsersEdit extends Component
         'email' => 'required|email|min:3',
         'password' => 'required|min:3',
         'team' => 'required|min:1|numeric',
+        'role' => 'required|min:1|numeric',
     ];
-
 
     public function mount($user)
     {
         $this->user = User::where('id', '=', $user->id)->whereNull('deleted_at')->first();
         $this->teams = Team::get();
+        $this->roles = Role::get();
         $this->name = $this->user->name;
         $this->email = $this->user->email;
         $this->team = $this->user->current_team_id;
+        $role = $user->roles()->first();
+        $this->role = $role != null ? $role->id : 0;
     }
 
     public function render()
@@ -52,6 +58,8 @@ class UsersEdit extends Component
                     'password' => bcrypt($this->password),
                     'current_team_id' => $this->team
                 ]);
+                $role = Role::where('id', '=', $this->role)->first();
+                $this->user->syncRoles([$role->name]);
                 $this->mount($this->user);
                 $this->emit('saved');
             }
